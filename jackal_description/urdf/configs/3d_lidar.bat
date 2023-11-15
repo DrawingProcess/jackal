fc281fe97bd24f20c0a0ae239de3b2b6912b4989
Seongjun Choi <sjchoi.dp@gmail.com>
2023-11-15 23:58:27 +0900

    Modify: topic name /velodyne_points to /cloud_in

--- jackal_description/urdf/configs/3d_lidar.bat
+++ jackal_description/urdf/configs/3d_lidar.bat
@@ -6,4 +6,5 @@ REM config, but with a SICK LMS100 series LIDAR on the front,
 REM pointing forward.
 
 set JACKAL_LASER_3D=1
-set JACKAL_LASER_3D_TOPIC="/velodyne_points"
+set JACKAL_LASER_3D_TOPIC="/cloud_in"
+@REM set JACKAL_LASER_3D_TOPIC="/velodyne_points"
